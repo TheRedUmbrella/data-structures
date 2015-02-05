2f45bae5e08ca5d5cd459119cbd436d5c31b6688
Christian A. Cruz Santiago <cacs107@gmail.com>
2015-02-04 21:44:15 -0500

    LinkedList started.

---
+++ DataStructures/src/dataStructures/Node.java
@@ -0,0 +1,26 @@
+package dataStructures;
+
+class Node {
+	private Node next;
+	private Object data;
+	
+	public Node(Object dataValue){
+		next = null;
+		data = dataValue;
+	}
+	
+	public Object getData(){
+		return data;
+	}
+	public void setData(Object dataValue){
+		data = dataValue;
+	}
+	
+	public Node getNext(){
+		return next;
+	}
+	
+	public void setNext(Node nextValue){
+		next = nextValue;
+	}
+}
